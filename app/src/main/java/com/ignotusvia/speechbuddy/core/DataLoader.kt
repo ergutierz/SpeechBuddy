5b5d1a11d1a43758539ce2e1e96722eabd281911
ergutierz <122296495+ergutierz@users.noreply.github.com>
2023-06-08 19:02:48 +0200

    Add DataLoader class

---
+++ app/src/main/java/com/ignotusvia/speechbuddy/core/DataLoader.kt
@@ -0,0 +1,8 @@
+import org.ndd4j.dataset.DataSet
+
+class DataLoader {
+    fun loadData(): DataSet {
+        // TODO: Implement this method
+    }
+    // other methods for preprocessing, data splitting, etc.
+}
