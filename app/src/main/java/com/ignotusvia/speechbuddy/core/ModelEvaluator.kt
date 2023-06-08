abb988c236930cf5e409b727945eeee064dbc023
ergutierz <122296495+ergutierz@users.noreply.github.com>
2023-06-08 19:04:15 +0200

    Add ModelEvaluator class

---
+++ app/src/main/java/com/ignotusvia/speechbuddy/core/ModelEvaluator.kt
@@ -0,0 +1,7 @@
+import org.ndd4j.model.Model
+class ModelEvaluator {
+    fun evaluateModel(): Model {
+        // TODO: Implement this method
+    }
+    // other methods for evaluation, validation, etc.
+}
