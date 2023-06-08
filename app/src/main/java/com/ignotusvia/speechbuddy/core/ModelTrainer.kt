eed0137a37c4ce21d23d1a949b7bb340258cd07c
ergutierz <122296495+ergutierz@users.noreply.github.com>
2023-06-08 19:03:43 +0200

    Add ModelTrainer class

---
+++ app/src/main/java/com/ignotusvia/speechbuddy/core/ModelTrainer.kt
@@ -0,0 +1,7 @@
+import org.ndd4j.model.Model
+class ModelTrainer {
+    fun trainModel(): Model {
+        // TODO: Implement this method
+    }
+    // other methods for training, validation, etc.
+}
